062c2ada8fd152cda7f052a0417c46011469a4ca
rMaxiQp <zq2@illinois.edu>
2017-03-30 08:49:57 -0500

    not sure if it is the correct way to implement it

--- tradingplatform/streamer.py
+++ tradingplatform/streamer.py
@@ -1,3 +1,8 @@
+import sys
+import getopt
+import Listener
+import PriceListener
+from yahoo_finance import Share
 # Streamer has a ticker symbol and an input file
 # It exposes the function `stream` which calls
 # reads the input file and yields the data as
@@ -14,14 +19,30 @@ class Streamer:
     # This function returns when the input file is
     # closed.
     def stream(self):
-        # implement this
-        pass
+        while (True):
+            line = input().strip()
+            with open(self.input_file, 'R') as info:
+                info.read()
 
 def main():
     # implement the code to parse command-line arguments,
     # create a Listener based on those arguments,
     # and start streaming the data
-    pass
-
+    listener_dict = {}
+    listener = None
+    try:
+        opts, args = getopt.getopt(argv,"hl",["listener"])
+    except getopt.GetoptError:
+        print('streamer.py -l <listener>')
+        sys.exit(2)
+    for opt.arg in opts:
+        if opt == '-h':
+            print ("streamer.py -l <listener>")
+        elif opt in ("-l", "--listener"):
+            listener = listener(arg)
+    if listener in listener_dict:
+        listener.stream()
+    else:
+        raise NotImplementedError
 if __name__ == "__main__":
     main()
