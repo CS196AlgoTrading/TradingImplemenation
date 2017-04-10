dc1b67980faf74dd0745f0101f8d2fe0bf31cfa7
yuyang <leon960809@gmail.com>
2017-04-09 23:48:15 -0500

    add some model feature

--- tradingplatform/models.py
+++ tradingplatform/models.py
@@ -1,10 +1,24 @@
-def compareToAvg(prices):
-    # Yields true if increasing, false if decreasing
-    historic = 0
-    i = 0
-    total = 0
-    for price in prices:
-        i += 1
-        total += price
-        historic = total/i
-        yield (price>historic)
+
+class models:
+    def __init__():
+        self.decisionScore = 0
+
+    def compareToAvg(currentprice,historicData):
+    # return true if increasing, false if decreasing
+        i = 0
+        total = 0
+        for price in historicData:
+            total += price
+
+        historic = total/(len(historicData))
+        return (currentprice > historic)
+
+    def makeDecision():
+
+        if compareToAvg(currentprice,historicData):
+            decisionScore ++
+        else:
+            decisionScore --
+
+        if decisionScore > 5 :
+            return "buy in"
