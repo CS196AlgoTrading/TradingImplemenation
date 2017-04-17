bdab3674640cf13dc4327e348398ef11c51a1801
rMaxiQp <zq2@illinois.edu>
2017-04-17 18:30:21 -0500

    add buyOrSell function to buy stocks or sell stocks

--- tradingplatform/models.py
+++ tradingplatform/models.py
@@ -8,3 +8,16 @@ def compareToAvg(prices):
         total += price
         historic = total/i
         yield (price>historic)
+
+def buyOrSell(ticker):
+    while True:
+        point = 0
+        increase = 0
+        decrease = 0
+        while point <= 50:
+            if compareToAvg(prices):
+                increase += 1
+            else:
+                decrease += 1
+            yield (increase >= decrease) #sell stocks Otherwise, buy stocks
+            point += 1
