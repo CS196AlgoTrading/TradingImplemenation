23645cf6b259940be3f4ce42d7ae2b49d75c0c03
rMaxiQp <zq2@illinois.edu>
2017-04-24 17:04:50 -0500

    modify models.py

--- tradingplatform/models.py
+++ tradingplatform/models.py
@@ -1,3 +1,6 @@
+import getopt
+import sys
+
 def compareToAvg(prices):
     # Yields true if increasing, false if decreasing
     historic = 0
@@ -9,15 +12,49 @@ def compareToAvg(prices):
         historic = total/i
         yield (price>historic)
 
-def buyOrSell(ticker):
+def buyOrSell(ticker, prices):
     while True:
         point = 0
         increase = 0
         decrease = 0
-        while point <= 50:
+        while point <= 50: #check 50 points of the average to decide
             if compareToAvg(prices):
                 increase += 1
             else:
                 decrease += 1
             yield (increase >= decrease) #sell stocks Otherwise, buy stocks
             point += 1
+
+def main(argv):
+    fileName = ""
+    amout = 0
+    ticker = ""
+        try:
+            opts, args = getopt.getopt(argv,"ht:a:p:f:,["ticker=","amount=", "profit=","fileName="])
+        except getopt.GetoptError:
+            print ('models.py -t <ticker> -a <amount> -p <profit> -f <file name>')
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print ('models.py -t <ticker> -a <amount> -p <profit>')
+                sys.exit()
+            elif opt in ("-t", "--ticker"):
+                ticker = arg
+            elif opt in ("-a", "--amount"):
+                print("amount of stocks:", amount)
+            elif opt in ("-p", "--profit"):
+                print("current profit:", profit)
+            elif opt in ("-f","--file name"):
+                fileName = argv
+
+    while(amount > -1):
+        if buyOrSell(ticker,prices):
+            amount += 100
+            profit -= amount * prices
+        else:
+            profit += amount * prices
+            amount = 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
