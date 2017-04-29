979380160c7ca7e3f763be361e77f1d83e81da82
rMaxiQp <zq2@illinois.edu>
2017-04-28 22:13:05 -0500

    update models.py

--- tradingplatform/models.py
+++ tradingplatform/models.py
@@ -1,61 +1,62 @@
 import getopt
 import sys
-import Streamer
+from streamer import Streamer
 
+historic = 0
 def compareToAvg(prices):
     # Yields true if increasing, false if decreasing
-    historic = 0
-    i = 0
+    number = 0
     total = 0
     for price in prices:
-        i += 1
+        number += 1
         total += price
-        historic = total/i
+        historic = total/number
         yield (price>historic)
 
-def buyOrSell(ticker, prices):
-    point = 0
+def tradingAmount(ticker):
     increase = 0
     decrease = 0
+    currentPrice = -1
+    prices = []
+    fileName = ticker + ".txt"
+    reader = Streamer(ticker, fileName)
+    for t in reader.stream(): # t for tuple
+        prices.append(t[2])
     for point in range (50): #check 50 points of the average to decide
         if compareToAvg(prices):
             increase += 1
         else:
             decrease += 1
-    return (increase >= decrease) #sell stocks Otherwise, buy stocks
+    if(increase > decrease):
+        currentPrice = prices[len(prices)-1]
+    return currentPrice
 
 def main(argv):
     fileName = ""
     holdingStock = 0
     ticker = ""
-        try:
-            opts, args = getopt.getopt(argv,"ht:h:p:,["ticker=","holdingStock=", "profit="])
-        except getopt.GetoptError:
-            print ('models.py -t <ticker> -a <amount> -p <profit>')
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt == '-h':
-                print ('models.py -t <ticker> -a <amount> -p <profit>')
-                sys.exit()
-            elif opt in ("-t", "--ticker"):
-                ticker = arg
-            elif opt in ("-h", "--amount"):
-                print("amount of holding stocks:", holdingStock)
-            elif opt in ("-p", "--profit"):
-                print("current profit:", profit)
-
-    fileName = ticker + ".txt"
-    streamer = Streamer(ticker, fileName)
-    prices = t[1] for t in streamer.stream() # t for tuple
+    profit = 0.0
+    try:
+        opts, args = getopt.getopt(argv,"ht:",["ticker="])
+    except getopt.GetoptError:
+        print ('models.py -t <ticker>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('models.py -t <ticker>')
+            sys.exit()
+        elif opt in ("-t", "--ticker"):
+            ticker = arg
 
     while(holdingStock > -1):
-        if buyOrSell(ticker,prices):
+        price = tradingAmount(ticker)
+        if holdingStock == 0 or price < 0:
             holdingStock += 100
-            profit -= holdingStock * prices * 1.1
+            profit -= holdingStock * price * 1.1
         else:
-            profit += holdingStock * prices * 0.9
+            profit += holdingStock * price * 0.9
             holdingStock = 0
-
+        print("amount of holding stocks: %d \tcurrent profit: %f" %(holdingStock,profit))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
