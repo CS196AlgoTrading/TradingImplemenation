f64b1efa5b630588f27e86edfa579266ea78de7f
leon9608 <leon960809@gmail.com>
2017-04-30 15:40:48 -0500

    Add files via upload

---
+++ tradingplatform/models.py
@@ -0,0 +1,62 @@
+import getopt
+import sys
+import time
+from streamer import Streamer
+
+totalnumber = 0
+historictotal = 0
+averageprice = 0
+
+def compareToAvg(price):
+    # Yields true if increasing, false if decreasing
+    global totalnumber
+    global historictotal
+    global averageprice
+    averageprice = historictotal/totalnumber
+    indicator = price > averageprice
+    totalnumber += 1
+    historictotal += price
+    return indicator
+
+def tradingAmount(ticker):
+    global averageprice
+    while(True){
+    fileName = ticker + ".txt"
+    reader = Streamer(ticker, fileName)
+    price = reader.stream();
+    if compareToAvg(price):
+        return price
+    else if price < averageprice:
+        return (0 - price)
+    time.sleep(10)
+    }
+
+def main(argv):
+    fileName = ""
+    holdingStock = 0
+    ticker = ""
+    profit = 0.0
+    try:
+        opts, args = getopt.getopt(argv,"ht:",["ticker="])
+    except getopt.GetoptError:
+        print ('models.py -t <ticker>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('models.py -t <ticker>')
+            sys.exit()
+        elif opt in ("-t", "--ticker"):
+            ticker = arg
+
+    while(holdingStock > -1):
+        price = tradingAmount(ticker)
+        if price > 0:
+            holdingStock += 100
+            profit -= holdingStock * price * 1.1
+        else:
+            profit -= holdingStock * price * 0.9
+            holdingStock = 0
+        print("amount of holding stocks: %d \tcurrent profit: %f" %(holdingStock,profit))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
